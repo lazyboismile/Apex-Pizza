0640e6d7b9e56bbe397c52136be99592426a57c2
lazyboismile <lazyboijon@gmail.com>
2025-10-18 22:45:57 +0500

    feat: Connecting to a Database & Getting Data

--- index.php
+++ index.php
@@ -1,5 +1,29 @@
 <?php
 
+    // connect to db
+    $connect = mysqli_connect('localhost', 'tonny', 'apexpizza', 'apex_pizza');
+
+    if(!$connect) {
+        echo 'Connection error: ' . mysqli_connect_error();
+    }
+
+    // write query for all pizzas
+	$sql = 'SELECT title, ingredients, id FROM pizzas ORDER BY created_at';
+
+	// get the result set (set of rows)
+	$result = mysqli_query($connect, $sql);
+
+	// fetch the resulting rows as an array
+	$pizzas = mysqli_fetch_all($result, MYSQLI_ASSOC);
+
+	// free the $result from memory (good practise)
+	mysqli_free_result($result);
+
+	// close connection
+	mysqli_close($connect);
+
+	print_r($pizzas);
+
 ?>
 
 <!DOCTYPE html>

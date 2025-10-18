e4503c9ad47c8fce83b4cc922034d429d2eb3602
lazyboismile <lazyboijon@gmail.com>
2025-10-18 22:49:03 +0500

    fix: Rendering Data to the Browser

--- index.php
+++ index.php
@@ -22,14 +22,37 @@
 	// close connection
 	mysqli_close($connect);
 
-	print_r($pizzas);
-
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
 
     <?php include('templates/header.php'); ?>
+
+    
+	<h4 class="center grey-text">Pizzas!</h4>
+
+	<div class="container">
+		<div class="row">
+
+			<?php foreach($pizzas as $pizza){ ?>
+
+				<div class="col s6 md3">
+					<div class="card z-depth-0">
+						<div class="card-content center">
+							<h6><?php echo htmlspecialchars($pizza['title']); ?></h6>
+							<div><?php echo htmlspecialchars($pizza['ingredients']); ?></div>
+						</div>
+						<div class="card-action right-align">
+							<a class="brand-text" href="#">more info</a>
+						</div>
+					</div>
+				</div>
+
+			<?php } ?>
+
+		</div>
+	</div>
 
     <?php include('templates/footer.php'); ?>
     

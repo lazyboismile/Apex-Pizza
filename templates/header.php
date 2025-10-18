8494269350677c9da2e20d68ffc8a279e0f2861d
lazyboismile <lazyboijon@gmail.com>
2025-10-18 17:21:19 +0500

    feat: create Forms & Filters and Validation

--- templates/header.php
+++ templates/header.php
@@ -9,6 +9,11 @@
   	.brand-text{
   		color: #cbb09c !important;
   	}
+    form{
+  		max-width: 460px;
+  		margin: 20px auto;
+  		padding: 20px;
+  	}
   </style>
 </head>
 <body class="grey lighten-4">
@@ -16,7 +21,7 @@
     <div class="container">
       <a href="#" class="brand-logo brand-text">Apex Pizza</a>
       <ul id="nav-mobile" class="right hide-on-small-and-down">
-        <li><a href="#" class="btn brand z-depth-0">Add a Pizza</a></li>
+        <li><a href="add.php" class="btn brand z-depth-0">Add a Pizza</a></li>
       </ul>
     </div>
   </nav>

616f754f9037f4307b23e5bb8e2dcac81767b2c1
lazyboismile <lazyboijon@gmail.com>
2025-10-19 22:47:35 +0500

    feat: Finishing Touches

--- config/db_connect.php
+++ config/db_connect.php
@@ -1,7 +1,7 @@
 <?php 
 
     // connect to db
-    $connect = mysqli_connect('localhost', 'tonny', 'apexpizza', 'apex_pizza');
+    $connect = mysqli_connect('localhost', 'Tonny', 'apexpizza', 'apex_pizza');
 
     if(!$connect) {
         echo 'Connection error: ' . mysqli_connect_error();
